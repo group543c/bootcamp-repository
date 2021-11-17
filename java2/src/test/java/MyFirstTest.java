5accfb38126a7ae99afab385832cbf8cbda7b17b
group543 <group543c@yandex.ru>
2021-11-17 10:11:36 +0900

    Задание 1.
    Инфраструктура + тест запуск браузера, открытие страницы и закрытие браузера Chrome/

---
+++ java2/src/test/java/MyFirstTest.java
@@ -0,0 +1,29 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MyFirstTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.navigate().to("https://software-testing.ru/");
+    }
+
+    @After
+    public void stop() {
+        driver.quit(); //остановить все ресурсы и процессы
+        driver = null;
+    }
+}
